2b6f99e2d816681376c9f14f5344e6242784374d
TewodrosAe <108419096+tewodrosAe@users.noreply.github.com>
2023-12-26 23:01:20 +0300

    Time: 37 ms (72.00%), Space: 17.7 MB (7.50%) - LeetHub

---
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        l, r = 0, 0
+        
+        while r < len(t) and l < len(s):
+            print(l)
+            if s[l] == t[r]:
+                l += 1
+            r += 1
+        
+        return l == len(s)
+        
