9a690b1b6698f922336d1ce7c153127defd14e52
TewodrosAe <108419096+tewodrosAe@users.noreply.github.com>
2023-12-30 23:07:06 +0300

    Time: 116 ms (59.25%), Space: 18.1 MB (9.66%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,34 +1,30 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
         # edge case 
-        if len(s) < len(t) or t == '':
-            return ""
+        if t == '' or len(t) > len(s):
+            return ''
         
-        # variables
-        res, resLen = [-1,-1], len(s) + 1
-        tDict = {}
+        window, substring = {}, {}
         l = 0
-        temp = {}  
-        count = 0
+        for w in t:
+            substring[w] = substring.get(w,0) + 1  
+        counter, subLength = 0, len(substring)
+        res, resLength = [-1,-1], len(s) + 1 
         
-        for char in t:
-            tDict[char] = tDict.get(char,0) + 1
-         
         for r in range(len(s)):
             c = s[r]
-            temp[c] = temp.get(c,0) + 1
+            window[c] = window.get(c,0) + 1
             
-            if c in tDict and temp[c] == tDict[c]:
-                count += 1
+            if c in substring and substring[c] == window[c]:
+                counter += 1
             
-            while count == len(tDict):
-                if resLen > r - l + 1:
+            while counter == subLength:
+                window[s[l]] -= 1
+                if r - l + 1 < resLength:
+                    resLength = r - l + 1
                     res = [l, r+1]
-                    resLen = r - l + 1
-                temp[s[l]] -= 1
-                if s[l] in tDict and tDict[s[l]] > temp[s[l]]:
-                    count -= 1
-                l += 1
+                if s[l] in substring and window[s[l]] < substring[s[l]]:
+                    counter -= 1
+                l += 1 
             
         return s[res[0]: res[1]]
-            
