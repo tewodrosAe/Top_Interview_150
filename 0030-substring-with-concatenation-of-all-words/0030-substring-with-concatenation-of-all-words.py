35090c526af16dd5fa940f91ec2beb7830badefc
TewodrosAe <108419096+tewodrosAe@users.noreply.github.com>
2023-12-30 18:56:42 +0300

    Time: 3350 ms (52.89%), Space: 18 MB (11.05%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -2,40 +2,30 @@ class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         if len(words) == 0:
             return []
-        # initialize d, l, ans
-        l = len(words[0])
-        d = {}
-        for w in words:
-            if w in d:
-                d[w] += 1
-            else:
-                d[w] = 1
-        i = 0
-        ans = []
-
-        # sliding window(s)
-        for k in range(l):
-            left = k
-            subd = {}
-            count = 0
-            for j in range(k, len(s)-l+1, l):
-                tword = s[j:j+l]
-                # valid word
-                if tword in d:
-                    if tword in subd:
-                        subd[tword] += 1
-                    else:
-                        subd[tword] = 1
-                    count += 1
-                    while subd[tword] > d[tword]:
-                        subd[s[left:left+l]] -= 1
-                        left += l
-                        count -= 1
-                    if count == len(words):
-                        ans.append(left)
-                # not valid
-                else:
-                    left = j + l
-                    subd = {}
-                    count = 0
-        return ans
+        wordLength = len(words[0])
+        concatLength = len(words[0]) * len(words)
+        wordsDict = {}
+        res = []
+        
+        for word in words:
+            wordsDict[word] = wordsDict.get(word,0) + 1
+        
+        l = 0
+        
+        while l < (len(s) - concatLength + 1):
+            wordsSeen = {}
+            
+            for r in range(l, l + concatLength, wordLength):
+                tempWord = s[r: r + wordLength]
+                if tempWord not in wordsDict:
+                    break
+                wordsSeen[tempWord] = wordsSeen.get(tempWord,0) + 1
+                if wordsSeen[tempWord] > wordsDict[tempWord]:
+                    word
+                    break
+            
+            if wordsSeen == wordsDict:
+                res.append(l)
+            l += 1
+            
+        return res
